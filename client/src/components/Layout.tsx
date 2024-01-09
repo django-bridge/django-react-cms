9705d1006bcc5241bbb965bedde6500ba5ad6ba2
Karl Hobley <karl@kaed.uk>
2024-01-09 23:08:17 +0000

    Use MUI for layout

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,77 +1,124 @@
 import { Link } from "@djream/core";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Box,
+  Drawer as MuiDrawer,
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  IconButton,
+  Typography,
+  List,
+  Container,
+} from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ArticleIcon from "@mui/icons-material/Article";
+
 import React from "react";
-import styled from "styled-components";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-flow: row;
-  height: 100vh;
-`;
+const drawerWidth: number = 240;
 
-const Sidebar = styled.div`
-  background-color: #535bf2;
-  color: white;
-  width: 150px;
-  display: flex;
-  flex-flow: column;
-`;
+const Drawer = styled(MuiDrawer)(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+    }),
+  },
+}));
 
-const Logo = styled(Link)`
-  font-size: 1.2rem;
-  font-weight: 700;
-  padding: 20px;
-  width: 100%;
-  text-align: center;
+const ListItemLink = styled(Link)`
   text-decoration: none;
-  color: white;
-  box-sizing: border-box;
-  background-color: rgba(1, 1, 1, 0);
-  transition: background-color 0.5s ease;
-
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.1);
-  }
+  color: inherit;
 `;
 
-const Menu = styled.ul`
-  width: 100%;
-`;
-
-const MenuItem = styled.li`
-  width: 100%;
-
-  > a {
-    display: block;
-    padding: 10px;
-    width: 100%;
-    text-decoration: none;
-    color: white;
-    box-sizing: border-box;
-    background-color: rgba(1, 1, 1, 0);
-    transition: background-color 0.5s ease;
-
-    &:hover {
-      background-color: rgba(255, 255, 255, 0.1);
-    }
-  }
-`;
-
-const Body = styled.div`
-  width: 100%;
-`;
+const defaultTheme = createTheme();
 
 export default function Layout({ children }: React.PropsWithChildren) {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Wrapper>
-      <Sidebar>
-        <Logo href="/">Djreampress</Logo>
-        <Menu>
-          <MenuItem>
-            <Link href="/posts/">Posts</Link>
-          </MenuItem>
-        </Menu>
-      </Sidebar>
-      <Body>{children}</Body>
-    </Wrapper>
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: "flex", flexFlow: "column", height: "100vh" }}>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Djreampress
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box sx={{ display: "flex", flexFlow: "row", height: "100%" }}>
+          <Drawer variant="permanent" open={open}>
+            <List component="nav" sx={{ flexGrow: 1 }}>
+              <ListItemLink href="/">
+                <ListItemButton>
+                  <ListItemIcon>
+                    <DashboardIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </ListItemLink>
+              <ListItemLink href="/posts/">
+                <ListItemButton>
+                  <ListItemIcon>
+                    <ArticleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Posts" />
+                </ListItemButton>
+              </ListItemLink>
+            </List>
+            <Box sx={{ marginLeft: "auto", marginRight: "auto" }}>
+              <IconButton onClick={toggleDrawer}>
+                <ChevronLeftIcon sx={{ ...(!open && { display: "none" }) }} />
+                <ChevronRightIcon sx={{ ...(open && { display: "none" }) }} />
+              </IconButton>
+            </Box>
+          </Drawer>
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: "100%",
+              overflow: "auto",
+            }}
+          >
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              {children}
+            </Container>
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
