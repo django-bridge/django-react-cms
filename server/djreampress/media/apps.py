19fea619762fcf34d030cc4b5f2c4da466a9b0a2
Karl Hobley <karl@kaed.uk>
2024-01-10 22:06:43 +0000

    Add app configs

---
+++ server/djreampress/media/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MediaConfig(AppConfig):
+    name = "djreampress.media"
+    label = "djreampress_media"
---
+++ server/djreampress/posts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    name = "djreampress.posts"
+    label = "djreampress_posts"
