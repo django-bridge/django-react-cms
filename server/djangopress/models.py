b1660637e686f8003910ef789d50059f78c51948
Karl Hobley <karl@kaed.uk>
2025-03-27 22:16:11 +0000

    Add Space model

--- server/djangopress/models.py
+++ server/djangopress/models.py
@@ -2,8 +2,21 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 
 
+class Space(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class SpaceMember(models.Model):
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
+    user = models.ForeignKey("djangopress.auth.User", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [("space", "user")]
+
+
 class Channel(models.Model):
     name = models.CharField(max_length=255)
+    space = models.ForeignKey(Space, on_delete=models.CASCADE, related_name="channels")
     channel_type = models.ForeignKey(ContentType, on_delete=models.PROTECT, related_name="+")
 
     def __str__(self):
